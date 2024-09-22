12c56875b4cccd162f7e90579bf5de4b622b2170
Kepha Kibet <Kibet.kepha@live.com>
2024-09-22 12:15:52 +0300

    Update PanelController.cs

--- blog/Controllers/PanelController.cs
+++ blog/Controllers/PanelController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace blog.Controllers
 {
-        [Authorize(Roles = "Admin")]
         //[Authorize(Roles = "Admin")]
     public class PanelController : Controller
     {
